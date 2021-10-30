4b977d5d82ebd9ac54fbb5c7c6c71bdb3d5713d6
“bryson” <brysonward77@gmail.com>
2021-10-29 22:23:51 -0400

    Changed db to Cleardb

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool ({
-    user: 'root',
-    password: 'Rrnyork212^',
-    host: 'localhost',
+    user: 'b1e67092acc09e',
+    password: '7a32826e',
+    host: 'us-cdbr-east-04.cleardb.com',
     port: 3306,
-    database: 'notes_db'
+    database: 'heroku_36234a15071467c'
 })
 
 module.exports = pool

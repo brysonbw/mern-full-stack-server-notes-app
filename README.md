442dc125b09bd11336c2e034895458eeb33b6aad
“bryson” <brysonward77@gmail.com>
2021-12-08 22:05:53 -0500

    MERN Full-Stack (Server) Notes App

---
+++ README.md
@@ -0,0 +1,19 @@
+MERN Full-Stack Notes App
+
+A note taking app that allows users to create, update, and delete notes.
+
+Tech Stack:
+
+Front-End/Client - React
+
+Back-End/Server - Node/Express
+
+Database - MySQL
+
+Technologies, Frameworks, & Languages:
+
+Javascript, HTML, CSS, (React)Bootstrap
+
+CRUD operations & fetching and storing data from custom built (backend/server) RESTful api to MySQL database
+
+_Server_
--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool ({
-    user: 'b1e67092acc09e',
-    password: '7a32826e',
-    host: 'us-cdbr-east-04.cleardb.com',
-    port: 3306,
-    database: 'heroku_36234a15071467c'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 })
 
 module.exports = pool
